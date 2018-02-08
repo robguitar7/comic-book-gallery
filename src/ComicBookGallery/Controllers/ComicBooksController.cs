6fcdc79fb1b62f3c8aadfb9b217f08a2c19d6176
Robert Guajardo <guajardorobert@live.com>
2018-02-08 15:58:40 -0600

    create a var for day

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,12 +10,14 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
+            var day = DateTime.Today.DayOfWeek;
+
             if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
             {
                 return Redirect("/");
               
             }
-            return Content("Hello from the comic books controller!");
+            return Content("Hello from the comic books controller! and Today is: "+ day);
         }
     }
 }
